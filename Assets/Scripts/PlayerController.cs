c3723e81b4ca59271ea7f64981fae093192237c7
Daniel <jared.fisher.2005@gmail.com>
2025-03-13 09:38:25 +0000

    test123

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public Transform holdPosition;
+    private Rigidbody rb;
+    private GameObject heldObject;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObject == null)
+                PickUpObject();
+            else
+                DropObject();
+        }
+    }
+
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
         
+        Vector3 moveDirection = new Vector3(moveX, 0, moveZ) * moveSpeed;
+        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+    }
+
+    void PickUpObject()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 2f);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag("PickUp"))
+            {
+                heldObject = collider.gameObject;
+                heldObject.transform.SetParent(holdPosition);
+                heldObject.transform.localPosition = Vector3.zero;
+                heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                break;
+            }
+        }
+    }
+
+    void DropObject()
+    {
+        if (heldObject != null)
+        {
+            heldObject.GetComponent<Rigidbody>().isKinematic = false;
+            heldObject.transform.SetParent(null);
+            heldObject = null;
+        }
     }
 }
--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public static class SoundManager {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public static void PlaySound() {
+        GameObject soundGameObject = new GameObject("Sound");
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+        //audioSource.PlayOneShot(GameAssets.i.playerWalk);
     }
 }
